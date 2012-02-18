185769eefb7f77b607f26420c94b10b5c03f0622
Leo Lapworth <leo@cuckoo.org>
2012-02-18 19:00:45 +0000

    Some other sys files

--- sys/installer.sh
+++ sys/installer.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Run a root
+# Run as root - manually probably easier than this script
+
+# Installing varnish:
+echo "To install varnish see:"
+echo "https://www.varnish-cache.org/installation/debian"
+echo "replace $(lsb_release -s -c) with: squeeze"
 
 # Copy in starman init
-cp etc/init.d/starman /etc/init.d/
+cp -r etc/* /etc/
 
 # Make sure it runs at startup
 /usr/sbin/update-rc.d starman defaults 18
 
-# 
