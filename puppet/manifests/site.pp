67c973a777b79464d248e5379a04d958ac04db12
Leo Lapworth <leo@cuckoo.org>
2013-01-03 20:59:01 +0000

    Perlbrew setup

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,6 +2,10 @@ node default {
     include sheep
     
     include sheep::ssh
+    
+    $perl = "perl-5.16.2"
+    sheep::perl {  $perl: }
+    
     include sheep::exim
     include sheep::configs
     include sheep::packages    
---
+++ puppet/modules/sheep/manifests/perl.pp
@@ -0,0 +1,25 @@
+define sheep::perl(
+    $version = $name,
+) {
+    
+    # Packages we need to build stuff
+    package { 
+        # for https
+        'libssl-dev': ensure => present;
+        # for gzip
+        'zlib1g-dev': ensure => present;
+        # for Test::XPath 
+        'libxml2-dev': ensure => present;
+        # for XML::Parser (used by Test::XPath)
+        'libexpat1-dev': ensure => present;
+        # AnyEvent::Curl::Multi
+        'libcurl4-openssl-dev': ensure => present;
+    }->
+
+    # install the perl
+    perlbrew::build { $version: }->
+
+    # install cpanm
+    perlbrew::install_cpanm { $version: }
+}
+
