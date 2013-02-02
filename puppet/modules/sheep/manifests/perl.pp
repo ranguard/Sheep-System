0b4b2f98ed35515e505aca544e90cfa43ea21a5b
Leo Lapworth <leo@cuckoo.org>
2013-02-02 21:37:01 +0000

    Perl for web

--- puppet/modules/sheep/manifests/perl.pp
+++ puppet/modules/sheep/manifests/perl.pp
@@ -21,5 +21,40 @@ define sheep::perl(
 
     # install cpanm
     perlbrew::install_cpanm { $version: }
+    
+    
+    # Modules for websites
+    perlbrew::install_module { [
+        "Business::PayPal"                             ,
+        "Catalyst::Action::RenderView"                 ,
+        "Catalyst::Authentication::Credential::HTTP"   ,
+        "Catalyst::Plugin::Cache"                      ,
+        "Catalyst::Plugin::Cache::Store::FastMmap"     ,
+        "Catalyst::Plugin::ConfigLoader"               ,
+        "Catalyst::Plugin::Session"                    ,
+        "Catalyst::Plugin::Session::State::Cookie"     ,
+        "Catalyst::Plugin::Session::Store::FastMmap"   ,
+        "Catalyst::Plugin::Static::Simple"             ,
+        "Catalyst::Runtime"                            ,
+        "Catalyst::View::TT"                           ,
+        "CHI"                                          ,
+        "Config::General"                              ,
+        "Data::UUID"                                   ,
+        "DateTime"                                     ,
+        "ExtUtils::MakeMaker"                          ,
+        "File::Temp"                                   ,
+        "Image::Imlib2::Thumbnail::S3"                 ,
+        "MIME::Types"                                  ,
+        "Moose"                                        ,
+        "MooseX::MethodAttributes::Role"               ,
+        "namespace::autoclean"                         ,
+        "Net::Amazon::S3"                              ,
+        "Net::FTP"                                     ,
+        "SimpleDB::Class"                              ,
+        "Test::More"                                   
+    ]:
+        perl => $sheep::perl,
+    }
+    
 }
 
